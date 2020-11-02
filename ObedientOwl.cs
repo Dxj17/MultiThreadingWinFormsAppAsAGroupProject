f19ce8fe80c11059c1e661df873206922ceef16c
Dominic Johnson <56883510+Dxj17@users.noreply.github.com>
2020-11-02 10:03:29 -0500

    Add files via upload

---
+++ ObedientOwl.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * 
+ * Dominic Johnson
+ * johnodx@mail.uc.edu
+ * Assignment 07
+ * Refrences:None 
+ * Have a fantastic beast of your choice after working w
+ */
+using System;
+using System.Threading;
+
+namespace MultiThreadingWinFormsAppAsAGroupProject {
+    /// <summary>
+    /// A Fantastic Beast that will help us maintain a secure world
+    /// </summary>
+    class ObedientOwl : FantasticBeast {
+        public override void SayHello() {
+
+            Console.WriteLine("Hello from " + this.GetType());
+        }
+        public override void RunThread()
+        {
+            // Use the value in request, compute the largest prime factor,
+            // store in response
+            msg = "Hello from AbiogeneticTestCase.RunThread and Obedient Owl()";
+            Thread.Sleep(2000);
+            long num = Convert.ToInt64(request);
+            response = Convert.ToString(19541);
+        }
+     
+          
+        
+
+    }
+}
